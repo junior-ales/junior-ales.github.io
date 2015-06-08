bce29c36b16ca8fd200517f4d669abfa351d951f
Junior Ales <edilson.ales.jr@gmail.com>
2015-06-08 16:28:36 -0400

    Adding webkit attributes for browser compatibilities

--- dev/javascript/app.js
+++ dev/javascript/app.js
@@ -4,6 +4,7 @@
     document.getElementById('title').style.display = 'none';
     viewMoreElem.style.display = 'none';
     contentElem.style.position = 'initial';
+    contentElem.style['-webkit-transform'] = 'translateY(0)';
     contentElem.style.transform = 'translateY(0)';
     setTimeout(function() {
       viewLessElem.style.display = 'block';
@@ -13,6 +14,7 @@
   var hideContent = function() {
     document.getElementById('social-media-buttons').style.display = null;
     document.getElementById('title').style.display = null;
+    contentElem.style["-webkit-transform"] = 'translateY(25%)';
     contentElem.style.transform = 'translateY(25%)';
     contentElem.style.position = 'fixed';
     viewLessElem.style.display = 'none';
