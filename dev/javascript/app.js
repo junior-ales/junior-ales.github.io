4690d7ca47abaad59937bef7f87fe56fdea5d5bf
Junior Ales <edilson.ales.jr@gmail.com>
2015-06-07 21:38:16 -0400

    Changing js selectors for compliance with old browsers

--- dev/javascript/app.js
+++ dev/javascript/app.js
@@ -1,24 +1,21 @@
-(function(supports) {
-  if (!supports) return;
-
-  var viewMoreElem = document.querySelector('#view-more');
-  var viewLessElem = document.querySelector('#view-less');
+(function() {
+  var viewMoreElem = document.getElementById('view-more');
+  var viewLessElem = document.getElementById('view-less');
 
   viewMoreElem.onclick = function() {
     viewLessElem.style.display = null;
-    document.querySelector('.social-media-buttons').style.display = 'none';
-    document.querySelector('.title').style.display = 'none';
-    document.querySelector('.content').style.display = null;
+    document.getElementById('social-media-buttons').style.display = 'none';
+    document.getElementById('title').style.display = 'none';
+    document.getElementById('content').style.display = null;
     viewMoreElem.style.display = 'none';
   };
 
   viewLessElem.onclick = function() {
     viewMoreElem.style.display = null;
-    document.querySelector('.social-media-buttons').style.display = null;
-    document.querySelector('.title').style.display = null;
-    document.querySelector('.content').style.display = 'none';
+    document.getElementById('social-media-buttons').style.display = null;
+    document.getElementById('title').style.display = null;
+    document.getElementById('content').style.display = 'none';
     viewLessElem.style.display = 'none';
   };
 
-
-})(!!document.querySelector && !!window.addEventListener);
+})();
