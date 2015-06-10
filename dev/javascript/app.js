10b3fab975b9a281154c22a725b1f43db448dcf3
Junior Ales <edilson.ales.jr@gmail.com>
2015-06-10 00:46:56 -0400

    Fix bug of changing mobile orientation while seeing the page

--- dev/javascript/app.js
+++ dev/javascript/app.js
@@ -5,7 +5,13 @@
       a = 'client';
       e = document.documentElement || document.body;
     }
-    return { width : e[ a+'Width' ] , height : e[ a+'Height' ] };
+    return {
+      width : e[ a+'Width' ],
+      height : e[ a+'Height' ],
+      largestDimension: function() {
+        return this.width > this.height ? this.width : this.height;
+      }
+    };
   })();
 
   var showContent = function() {
@@ -23,16 +29,16 @@
   var hideContent = function() {
     document.getElementById('social-media-buttons').style.display = null;
     document.getElementById('title').style.display = null;
-    contentElem.style['-webkit-transform'] = 'translateY('+ viewport.height+ 'px)';
-    contentElem.style.transform = 'translateY('+ viewport.height+ 'px)';
+    contentElem.style['-webkit-transform'] = 'translateY('+ viewport.largestDimension()+ 'px)';
+    contentElem.style.transform = 'translateY('+ viewport.largestDimension()+ 'px)';
     contentElem.style.position = 'fixed';
     viewLessElem.style.display = 'none';
     viewMoreElem.style.display = null;
   };
 
   var contentElem = document.getElementById('content');
-  contentElem.style['-webkit-transform'] = 'translateY('+ viewport.height+ 'px)';
-  contentElem.style.transform = 'translateY('+ viewport.height+ 'px)';
+  contentElem.style['-webkit-transform'] = 'translateY('+ viewport.largestDimension()+ 'px)';
+  contentElem.style.transform = 'translateY('+ viewport.largestDimension()+ 'px)';
   setTimeout(function() {
     contentElem.style.visibility = 'visible';
   }, 550);
