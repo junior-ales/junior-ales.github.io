aa46c6e33e4d49c4d7e1b558e0b90eb90d68c0c2
Junior Ales <edilson.ales.jr@gmail.com>
2015-06-13 20:38:31 -0400

    Sending email via AJAX

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -40,18 +40,7 @@
     viewMoreElem.style.display = null;
   };
 
-  var contentElem = document.getElementById('content');
-  contentElem.style['-webkit-transform'] = 'translateY('+ viewport.largestDimension()+ 'px)';
-  contentElem.style['-moz-transform'] = 'translateY('+ viewport.largestDimension()+ 'px)';
-  contentElem.style['-ms-transform'] = 'translateY('+ viewport.largestDimension()+ 'px)';
-  contentElem.style['-o-transform'] = 'translateY('+ viewport.largestDimension()+ 'px)';
-  contentElem.style.transform = 'translateY('+ viewport.largestDimension()+ 'px)';
-  setTimeout(function() {
-    contentElem.style.visibility = 'visible';
-  }, 550);
-
-  var emailLinkClick = function() {
-    showContent();
+  var emailLinkClick = function() { showContent();
     setTimeout(function() {
       document.getElementById('email-me').scrollIntoView();
     }, 550);
@@ -65,11 +54,61 @@
     document.getElementById('view-less').style.opacity = 0.2;
   };
 
+  var sendEmail = function(e) {
+    e.preventDefault();
+
+    function encodeJson(object) { // copied from http://blog.garstasio.com/you-dont-need-jquery/ajax/#url-encoding
+      var encodedString = '';
+      for (var prop in object) {
+        if (object.hasOwnProperty(prop)) {
+          if (encodedString.length > 0) {
+            encodedString += '&';
+          }
+          encodedString += encodeURI(prop + '=' + object[prop]);
+        }
+      }
+      return encodedString;
+    }
+
+    function sendEmailHandler(req) {
+      return function() {
+        if (req.status === 200 && req.responseText.success) {
+          console.log("e foi meso");
+        }
+      };
+    }
+
+    var data = encodeJson({
+      message: senderMessage.value,
+      _replyto: senderEmail.value,
+      _subject: "test pra valer",
+      _gotcha: document.getElementById('sender-gotcha').value
+    });
+
+    var request = new XMLHttpRequest();
+    request.open('POST', '//formspree.io/edilson.ales.jr@gmail.com', true);
+    request.setRequestHeader("Accept", "application/json, text/javascript, */*; q=0.01");
+    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    request.onload = sendEmailHandler(request);
+    request.send(data);
+  };
+
+  var contentElem = document.getElementById('content');
   var viewMoreElem = document.getElementById('view-more');
   var viewLessElem = document.getElementById('view-less');
   var emailLink = document.getElementById('email-link');
   var senderEmail = document.getElementById('sender-email');
   var senderMessage = document.getElementById('sender-message');
+  var senderEmailButton = document.getElementById('send-email-button');
+
+  contentElem.style['-webkit-transform'] = 'translateY('+ viewport.largestDimension()+ 'px)';
+  contentElem.style['-moz-transform'] = 'translateY('+ viewport.largestDimension()+ 'px)';
+  contentElem.style['-ms-transform'] = 'translateY('+ viewport.largestDimension()+ 'px)';
+  contentElem.style['-o-transform'] = 'translateY('+ viewport.largestDimension()+ 'px)';
+  contentElem.style.transform = 'translateY('+ viewport.largestDimension()+ 'px)';
+  setTimeout(function() {
+    contentElem.style.visibility = 'visible';
+  }, 550);
 
   viewMoreElem.onclick = showContent;
   viewLessElem.onclick = hideContent;
@@ -78,5 +117,5 @@
   senderMessage.onfocus = decreaseOpacityViewLess;
   senderEmail.onblur = increaseOpacityViewLess;
   senderMessage.onblur = increaseOpacityViewLess;
-
+  senderEmailButton.onclick = sendEmail;
 })();
