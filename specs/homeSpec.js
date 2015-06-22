fea0d5f4626688227fa6baf6a628e34c49f98428
Junior Ales <edilson.ales.jr@gmail.com>
2015-06-22 00:26:20 -0400

    Adding first selenium test

--- specs/homeSpec.js
+++ specs/homeSpec.js
@@ -1,42 +1,54 @@
-process.env.NODE_ENV = 'test';
 var app = require(__dirname + '/../app');
 var assert = require('assert');
-var Browser = require('zombie');
+var webdriver = require('selenium-webdriver');
+var test = require('selenium-webdriver/testing');
+var By = webdriver.By;
 
-describe('Home Page', function() {
-
-  before(function() {
+test.describe('Home Page', function() {
+  test.before(function(done) {
     this.server = require('http').createServer(app).listen(3000);
-    this.browser = new Browser({ site: 'http://localhost:3000' });
+    this.driver = new webdriver.Builder().forBrowser('phantomjs').build();
+    done();
   });
 
-  beforeEach(function(done) {
-    this.browser.visit('/', done);
+  test.after(function(done) {
+    this.server.close();
+    this.driver.quit();
+    done();
   });
 
-  it('should have have my name =\)', function() {
-    assert.ok(this.browser.success);
-    assert.equal(this.browser.text('#title h1'), 'junior ales');
+  test.beforeEach(function(done) {
+    this.driver.get('http://localhost:3000');
+    done();
   });
 
-  it('should have links of main social media');
-  it('should have a link to contact form');
-  it('should have a button to see MORE content');
-  it('should not have a button to see LESS content');
+  test.it('should have my name =\)', function(done) {
+    var elem = this.driver.findElement(By.css('#title h1'));
+    elem.getText().then(function(text) {
+      assert.equal(text, 'junior ales');
+      done();
+    });
+  });
 
-  describe('more content', function() {
-    it('should have a button to see LESS content');
-    it('should not have a button to see MORE content');
-    it('should have a summary');
-    it('should have a personal blog section');
-    it('should have a twitter section');
-    it('should have a footer with links of social media and the github project');
+  test.it('should have a links of main social media');
+  test.it('should have a link to contact form');
+  test.it('should have a button to see MORE content');
+  test.it('should not have a button to see LESS content');
+  test.it('should not show the the elements of more content section');
 
-    describe('contact form', function() {
-      it('should show a success message when sending email');
-      it('should show an error message when form is empty');
-      it('should show an error message when email address is missing');
-      it('should show an error message when a message is missing');
+  test.describe('more content', function() {
+    test.it('should have a button to see LESS content');
+    test.it('should not show elements of the initial view');
+    test.it('should have a summary');
+    test.it('should have a personal blog section');
+    test.it('should have a twitter section');
+    test.it('should have a footer with links of social media and the github project');
+
+    test.describe('contact form', function() {
+      test.it('should show a success message when sending email');
+      test.it('should show an error message when form is empty');
+      test.it('should show an error message when email address is missing');
+      test.it('should show an error message when a message is missing');
     });
   });
 });
