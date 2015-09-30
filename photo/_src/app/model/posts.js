dc300102bf1c53c873d655342002ffad361ff623
Junior Ales <edilson.ales.jr@gmail.com>
2015-09-29 22:13:03 -0300

    [photo] renaming posts and finishing transalations

--- photo/_src/app/model/posts.js
+++ photo/_src/app/model/posts.js
@@ -3,11 +3,11 @@
 module.exports = [
   {
     "id": 0,
-    "name": "lunch-by-the-bay",
-    "title": "Lunch by the Bay",
+    "name": "o-almoco-e-a-baia",
+    "title": "O Almoço e a baía",
     "dir": "posts/2015/05/22/",
     "htmlContent": "<p>Não é difícil ver alguém que bate no peito e diz com maior orgulho Bitters health goth cardigan, vinyl listicle street art letterpress gastropub meggings Vice sartorial cred. Fap Shoreditch lumbersexual semiotics deep v mlkshk. Ethical sartorial pour-over artisan, chia typewriter cronut messenger bag single-origin coffee mlkshk Brooklyn. Raw denim cold-pressed McSweeney\"s cardigan, blog put a bird on it meditation forage freegan bitters High Life dreamcatcher whatever. Swag asymmetrical cronut keffiyeh. Umami Odd Future Bushwick, pop-up artisan mustache paleo migas irony organic tote bag sartorial slow-carb. Direct trade retro chillwave sustainable street art, Vice sartorial.</p>",
-    "location": "San Francisco, USA",
+    "location": "São Francisco, EUA",
     "pubdate": new Date("2015-05-23"),
     "viewsRanking": 1
   },
@@ -25,34 +25,12 @@ module.exports = [
 
   {
     "id": 2,
-    "name": "floating-flowers",
-    "title": "Floating Flowers",
+    "name": "flores-flutuantes",
+    "title": "Flores Flutuantes",
     "dir": "posts/2015/07/17/",
     "htmlContent": "<p>Essas foram as palavras de um colega numa despretensiosa pausa para um café no escritório que me fizeram refletir bastante. Por algumas horas fiquei me questionando 'será que ele está errado?'. Responder a essa pergunta me tomou algum tempo e ao final, a linha de raciocínio me pareceu mais interessante do que a resposta em si.</p> <p>Ele, assim como eu, é homem, branco, de classe média, empregado numa multinacional e teve tempo e apoio familiar suficientes para se preparar para o mercado de trabalho. As melhores oportunidades da nossa sociedade estão disponíveis para ele e para mim. Somos a minoria privilegiada desse país que reclama do imposto para trazer o eletrônico estrangeiro e do outro para fazer uma comprinha com cartão de crédito internacional.</p> <p>Reclamamos da dificuldade em achar alguém para limpar nossa casa, de como está cara a entrada da balada e de como não se fazem mais mulheres como antigamente.</p>",
-    "location": "Amsterdam, Netherlands",
+    "location": "Amsterdã, Holanda",
     "pubdate": new Date("2015-07-18"),
     "viewsRanking": 2
-  },
-
-  {
-    "id": 3,
-    "name": "lunch-by-the-bay",
-    "title": "Lunch by the Bay",
-    "dir": "posts/2015/05/22/",
-    "htmlContent": "<p>Não é difícil ver alguém que bate no peito e diz com maior orgulho Bitters health goth cardigan, vinyl listicle street art letterpress gastropub meggings Vice sartorial cred. Fap Shoreditch lumbersexual semiotics deep v mlkshk. Ethical sartorial pour-over artisan, chia typewriter cronut messenger bag single-origin coffee mlkshk Brooklyn. Raw denim cold-pressed McSweeney\"s cardigan, blog put a bird on it meditation forage freegan bitters High Life dreamcatcher whatever. Swag asymmetrical cronut keffiyeh. Umami Odd Future Bushwick, pop-up artisan mustache paleo migas irony organic tote bag sartorial slow-carb. Direct trade retro chillwave sustainable street art, Vice sartorial.</p>",
-    "location": "San Francisco, USA",
-    "pubdate": new Date("2015-05-19"),
-    "viewsRanking": 5
-  },
-
-  {
-    "id": 4,
-    "name": "origami-party",
-    "title": "Origami Party",
-    "dir": "posts/2015/04/13/",
-    "htmlContent": "<p>Essas foram as palavras de um colega numa despretensiosa pausa para um café no escritório que me fizeram refletir bastante. Por algumas horas fiquei me questionando 'será que ele está errado?'. Responder a essa pergunta me tomou algum tempo e ao final, a linha de raciocínio me pareceu mais interessante do que a resposta em si.</p>",
-    "location": "Pune, India",
-    "pubdate": new Date("2015-04-11"),
-    "viewsRanking": 4
   }
 ];
