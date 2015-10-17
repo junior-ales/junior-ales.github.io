02b08707e1b855bc2a28e9ad170b227d950abf5d
Junior Ales <edilson.ales.jr@gmail.com>
2015-10-17 17:59:59 -0300

    [photo] flores flutuantes post done

--- photo/_src/app/model/posts.js
+++ photo/_src/app/model/posts.js
@@ -1,5 +1,11 @@
 "use strict";
 
+var pubdate = function pubdate(dateString) {
+  var date = new Date(dateString);
+  date.setTime(date.getTime() + date.getTimezoneOffset()*60*1000);
+  return date;
+}
+
 module.exports = [
   {
     "id": 0,
@@ -8,7 +14,7 @@ module.exports = [
     "dir": "posts/2015/05/22/",
     "htmlContent": "<p>Não é difícil ver alguém que bate no peito e diz com maior orgulho Bitters health goth cardigan, vinyl listicle street art letterpress gastropub meggings Vice sartorial cred. Fap Shoreditch lumbersexual semiotics deep v mlkshk. Ethical sartorial pour-over artisan, chia typewriter cronut messenger bag single-origin coffee mlkshk Brooklyn. Raw denim cold-pressed McSweeney\"s cardigan, blog put a bird on it meditation forage freegan bitters High Life dreamcatcher whatever. Swag asymmetrical cronut keffiyeh. Umami Odd Future Bushwick, pop-up artisan mustache paleo migas irony organic tote bag sartorial slow-carb. Direct trade retro chillwave sustainable street art, Vice sartorial.</p>",
     "location": "São Francisco, EUA",
-    "pubdate": new Date("2015-05-23"),
+    "pubdate": pubdate("2015-05-22"),
     "viewsRanking": 1
   },
 
@@ -19,7 +25,7 @@ module.exports = [
     "dir": "posts/2015/04/13/",
     "htmlContent": "<p>Bitters health goth cardigan,</p> <p>vinyl listicle street art letterpress gastropub meggings Vice sartorial cred. Fap Shoreditch lumbersexual semiotics deep v mlkshk. Ethical sartorial pour-over artisan, chia typewriter cronut messenger bag single-origin coffee mlkshk Brooklyn. Raw denim cold-pressed McSweeney\"s cardigan, blog put a </p><p>bird on it meditation forage freegan bitters High Life dreamcatcher whatever. Swag asymmetrical cronut keffiyeh. Umami Odd Future Bushwick, pop-up artisan mustache paleo migas irony organic tote bag sartorial slow-carb. Direct trade retro chillwave sustainable street art, Vice sartorial.</p>",
     "location": "Pune, India",
-    "pubdate": new Date("2015-04-14"),
+    "pubdate": pubdate("2015-04-13"),
     "viewsRanking": 3
   },
 
@@ -27,10 +33,26 @@ module.exports = [
     "id": 2,
     "name": "flores-flutuantes",
     "title": "Flores Flutuantes",
-    "dir": "posts/2015/07/17/",
-    "htmlContent": "<p>Essas foram as palavras de um colega numa despretensiosa pausa para um café no escritório que me fizeram refletir bastante. Por algumas horas fiquei me questionando 'será que ele está errado?'. Responder a essa pergunta me tomou algum tempo e ao final, a linha de raciocínio me pareceu mais interessante do que a resposta em si.</p> <p>Ele, assim como eu, é homem, branco, de classe média, empregado numa multinacional e teve tempo e apoio familiar suficientes para se preparar para o mercado de trabalho. As melhores oportunidades da nossa sociedade estão disponíveis para ele e para mim. Somos a minoria privilegiada desse país que reclama do imposto para trazer o eletrônico estrangeiro e do outro para fazer uma comprinha com cartão de crédito internacional.</p> <p>Reclamamos da dificuldade em achar alguém para limpar nossa casa, de como está cara a entrada da balada e de como não se fazem mais mulheres como antigamente.</p>",
+    "dir": "posts/2015/10/17/",
+    "htmlContent": "<p> \
+      Em frente um dos cartões postais mais famosos do mundo, um espelho d'água. \
+      Flores parecem brotar dele, parecem estar soltas, dançando ao ritmo do vento na coluna d'água. \
+      Centenas de pessoas tirando e posando para fotos nesse mesmo lugar que eu estava, porém, \
+      a direção de suas câmeras era diferente da direção da minha.\
+      </p><p> \
+      O que chamava a atenção de tanta gente para o lado oposto era o letreiro gigante com os dizeres \
+      <a class='post-content__link' href='https://www.google.com/search?q=iamsterdam&es_sm=119&source=lnms&tbm=isch&sa=X&biw=1280&bih=702' target='_blank'> \
+      'I AMSTERDAM'</a> bem em frente ao <i>Rijksmuseum</i>. \
+      Não que o letreiro não me encantasse, é bem bonito realmente, mas as cores vivas e quentes das flores \
+      nesse espelho d'água quase que gritavam por um pouco de atenção naquele dia frio e de vento cortante. \
+      Não pude resistir ao chamado delas e o que acabou acontecendo foi que não tenho nenhuma foto na frente do tal letreiro. \
+      </p><p> \
+      Fui embora dali, fiquei mais uns dias na cidade, mas fotografar as belas paisagens de Amsterdã \
+      ficou um pouco mais difícil depois dessa foto. Nada conseguia trazer de novo esse sentimento de 'belo ignorado' \
+      que essas flores me fizeram sentir. \
+      </p>",
     "location": "Amsterdã, Holanda",
-    "pubdate": new Date("2015-07-18"),
+    "pubdate": pubdate("2015-10-17"),
     "viewsRanking": 2
   }
 ];
