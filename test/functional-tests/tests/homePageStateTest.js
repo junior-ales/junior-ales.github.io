056c5575d951dcb07519f65d3576712892ed745f
Junior Ales <edilson.ales.jr@gmail.com>
2015-11-02 23:52:56 -0200

    [cover] making e2e tests faster

--- test/functional-tests/tests/homePageStateTest.js
+++ test/functional-tests/tests/homePageStateTest.js
@@ -1,23 +1,21 @@
 module.exports = {
-  'Hide Email Link in Small Screens' : function (browser) {
-    var coverpage = browser.page.coverpage();
-
-    browser.maximizeWindow();
-    coverpage.navigate().assert.visible('@emailHeaderLink');
-
-    browser.resizeWindow(320, 480);
-    browser.pause(500);
-    coverpage.navigate().assert.hidden('@emailHeaderLink');
-
-    browser.end();
-  },
-
   'Expand and Collapse Details' : function(browser) {
     var coverpage = browser.page.coverpage();
     coverpage.
       navigate().
       expandDetails().
       collapseDetails();
+  },
+
+  'Hide Email Link in Small Screens' : function (browser) {
+    var coverpage = browser.page.coverpage();
+
+    browser.maximizeWindow();
+    coverpage.assert.visible('@emailHeaderLink');
+
+    browser.resizeWindow(320, 480);
+    browser.pause(500);
+    coverpage.assert.hidden('@emailHeaderLink');
 
     browser.end();
   }
--- test/functional-tests/tests/sendEmailTest.js
+++ test/functional-tests/tests/sendEmailTest.js
@@ -1,5 +1,5 @@
 module.exports = {
-  'Send Email Successfully' : function (browser) {
+  'Transparentize Collapse Details Button' : function (browser) {
     var coverpage = browser.page.coverpage();
 
     coverpage.
@@ -8,20 +8,25 @@ module.exports = {
       assertOpacity('@collapseDetails', '1').
       click('@emailContent').
       assertOpacity('@collapseDetails', '0.2').  // thanks to safari I can't use assert.cssProperty('opacity', '0.2')
+      click('@sendEmailButton').
+      assertOpacity('@collapseDetails', '1');
+  },
+
+  'Send Email Successfully' : function (browser) {
+    var coverpage = browser.page.coverpage();
+
+    coverpage.
+      navigate().
+      expandDetails().
       sendEmail('email@test.com', 'an email content').
-      assertOpacity('@collapseDetails', '1').
       assertAlertMessage('Email Sent Successfuly').
       assertEmailFieldsAreEmpty();
-
-    browser.end();
   },
 
   'Send Email Failure' : function (browser) {
     var coverpage = browser.page.coverpage();
 
     coverpage.
-      navigate().
-      expandDetails().
       sendEmail().
       assertAlertMessage('Please add a valid email and a message').
       sendEmail('INVALID EMAIL ADDRESS', 'an email content').
