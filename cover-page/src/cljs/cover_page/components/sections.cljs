216ce449768b74335c4b3f3c9ed8bb80f1f9c35a
Junior Ales <edilson.ales.jr@gmail.com>
2016-03-03 19:36:59 -0300

    [cover] refactoring sections SASS

--- cover-page/src/cljs/cover_page/components/sections.cljs
+++ cover-page/src/cljs/cover_page/components/sections.cljs
@@ -2,13 +2,13 @@
   (:require [cover-page.utils.content :refer [label]]))
 
 (defn sections []
-  [:section#content
-   [:article.content-block.summary
-    [:header.block-header
-     [:h1 (label :sections-summary-title)]
-     [:p (label :sections-summary-subtitle)]]
-    [:section.block-description
+  [:section.sections-container
+   [:article.summary-section
+    [:header.section-header
+     [:h1.title (label :sections-summary-title)]
+     [:p.subtitle (label :sections-summary-subtitle)]]
+    [:section.section-description
      [:p.description (label :sections-summary-desc)]
-     [:p.avatar
-      [:img {:src "images/avatar.jpg" :alt "Junior Ales' avatar"}]]]]])
+     [:p.avatar-wrapper
+      [:img.avatar {:src "images/avatar.jpg" :alt "Junior Ales' avatar"}]]]]])
 
