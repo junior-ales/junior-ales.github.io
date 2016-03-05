75db282352d37844c6a771416e660267c0c9827d
Junior Ales <edilson.ales.jr@gmail.com>
2016-03-05 19:26:53 -0300

    [cover] adding photo blog section

--- cover-page/src/cljs/cover_page/components/sections.cljs
+++ cover-page/src/cljs/cover_page/components/sections.cljs
@@ -1,8 +1,7 @@
 (ns cover-page.components.sections
   (:require [cover-page.utils.content :refer [label]]))
 
-(defn sections []
-  [:section.sections-container.hidden {:data-appear-order 2}
+(defn summary []
    [:article.summary-section
     [:header.section-header
      [:h1.title (label :sections-summary-title)]
@@ -10,5 +9,26 @@
     [:section.section-description
      [:p.description (label :sections-summary-desc)]
      [:p.avatar-wrapper
-      [:img.avatar {:src "images/avatar.jpg" :alt "Junior Ales' avatar"}]]]]])
+      [:img.avatar {:src "images/avatar.jpg"
+                    :alt (label :sections-summary-avatar-alt)}]]]])
+
+(defn photoblog-section []
+  [:article.photoblog-section
+   [:header.section-header
+    [:h1.title (label :sections-photoblog-title)]
+    [:p.subtitle (label :sections-photoblog-subtitle)]]
+   [:section.section-description
+    [:p.description (label :sections-photoblog-desc)]
+    [:p.avatar-wrapper
+     [:a.avatar-link {:target "_blank"
+                      :href "http://www.juniorales.com/photo"}
+      [:img.avatar {:src "images/photo-blog-icon.jpg"
+                    :alt (label :sections-photoblog-avatar-alt)}]
+      [:span "juniorales.com/photo"]]]]])
+
+(defn sections []
+  [:section.sections-container.hidden
+   {:data-appear-order 2}
+   [summary]
+   [photoblog-section]])
 
--- cover-page/src/cljs/cover_page/utils/content.cljs
+++ cover-page/src/cljs/cover_page/utils/content.cljs
@@ -9,6 +9,11 @@
            :sections-summary-title "Apresentação"
            :sections-summary-subtitle "uma pequena descrição sobre mim"
            :sections-summary-desc "Um consultor de desenvolvimento de software resolvendo problemas de pessoas com código e ideias. Encontre aqui todo o conteúdo que produzo na web e as maneiras de me contactar. Você gosta de tecnologia, desenvolvimento de software, jiu jitsu brasileiro ou fotografia? Vamos conversar."
+           :sections-summary-avatar-alt "Retrato de Junior Ales"
+           :sections-photoblog-title "Blog de Fotos"
+           :sections-photoblog-subtitle "fotografia, blog, inspiração"
+           :sections-photoblog-desc "Gosto de pensar a fotografia além de equipamentos, técnicas e cores. O que mais me intriga é a história por trás das lentes. Acredito que fotografar seja sobre a maneira única que as pessoas veem e sentem o mundo em sua volta naquele instante. Esse blog é a maneira que encontrei de contar minhas histórias."
+           :sections-photoblog-avatar-alt "Ícone do meu blog de fotos"
            :back "voltar"
            :view-more "ver mais"}
    :en-us {:title-description "consultant developer"
@@ -16,6 +21,11 @@
            :sections-summary-title "Summary"
            :sections-summary-subtitle "brief description about me"
            :sections-summary-desc "A consultant of software development solving people's problems with code and ideas. Find here all the content I produce in the web and the ways to contact me. Do you like technology, software development, brazilian jiu jitsu or photography? Let's talk."
+           :sections-summary-avatar-alt "Junior Ales' portrait"
+           :sections-photoblog-title "Photo Blog"
+           :sections-photoblog-subtitle "photography, blog, inspiration"
+           :sections-photoblog-desc "I like to think of photography beyond techniques, colors, focus, depth and patterns. I'm fascinated about the story behind the lens. It's all about the unique way people see and feel the world around them in that given moment. This blog is a way of telling my stories to everyone. The content of the blog is (for now) only in portuguese, but I believe non portuguese speakers will be able to understand my ideas through my photos."
+           :sections-photoblog-avatar-alt "Icon of my photo blog"
            :back "back"
            :view-more "view more"}})
 
