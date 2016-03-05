14f2f0dc132f46831c2a7e3ba870f61ff85bec27
Junior Ales <edilson.ales.jr@gmail.com>
2016-03-05 20:45:18 -0300

    [cover] adding twitter section

--- cover-page/src/cljs/cover_page/components/sections.cljs
+++ cover-page/src/cljs/cover_page/components/sections.cljs
@@ -1,6 +1,28 @@
 (ns cover-page.components.sections
   (:require [cover-page.utils.content :refer [label]]))
 
+(def section-props
+  [{:key 12 :name "photoblog" :href "http://www.juniorales.com/photo"}
+   {:key 13 :name "twitter" :href "http://www.twitter.com/junior_ales"}])
+
+(defn avatar [])
+(defn link-avatar [])
+
+(defn section [props]
+  (let [avatar-path (str "images/" (:name props) "-icon.jpg")
+        key-for #(keyword (str "sections-" (:name props) "-" %))]
+    [:article {:key (:key props) :class (str (:name props) "-section")}
+     [:header.section-header
+      [:h1.title (label (key-for "title"))]
+      [:p.subtitle (label (key-for "subtitle"))]]
+     [:section.section-description
+      [:p.description (label (key-for "desc"))]
+      [:p.avatar-wrapper
+       [:a.avatar-link {:target "_blank" :href (:href props)}
+        [:img.avatar {:src avatar-path
+                      :alt (label (key-for "avatar-alt"))}]
+        [:span.text (subs (:href props) 11)]]]]]))
+
 (defn summary []
    [:article.summary-section
     [:header.section-header
@@ -12,23 +34,9 @@
       [:img.avatar {:src "images/avatar.jpg"
                     :alt (label :sections-summary-avatar-alt)}]]]])
 
-(defn photoblog-section []
-  [:article.photoblog-section
-   [:header.section-header
-    [:h1.title (label :sections-photoblog-title)]
-    [:p.subtitle (label :sections-photoblog-subtitle)]]
-   [:section.section-description
-    [:p.description (label :sections-photoblog-desc)]
-    [:p.avatar-wrapper
-     [:a.avatar-link {:target "_blank"
-                      :href "http://www.juniorales.com/photo"}
-      [:img.avatar {:src "images/photo-blog-icon.jpg"
-                    :alt (label :sections-photoblog-avatar-alt)}]
-      [:span "juniorales.com/photo"]]]]])
-
 (defn sections []
   [:section.sections-container.hidden
    {:data-appear-order 2}
    [summary]
-   [photoblog-section]])
+   (doall (map section section-props))])
 
--- cover-page/src/cljs/cover_page/utils/content.cljs
+++ cover-page/src/cljs/cover_page/utils/content.cljs
@@ -6,26 +6,43 @@
 (def content
   {:pt-br {:title-description "consultor de desenvolvimento"
            :lang-button "English?"
+
            :sections-summary-title "Apresentação"
            :sections-summary-subtitle "uma pequena descrição sobre mim"
            :sections-summary-desc "Um consultor de desenvolvimento de software resolvendo problemas de pessoas com código e ideias. Encontre aqui todo o conteúdo que produzo na web e as maneiras de me contactar. Você gosta de tecnologia, desenvolvimento de software, jiu jitsu brasileiro ou fotografia? Vamos conversar."
            :sections-summary-avatar-alt "Retrato de Junior Ales"
+
            :sections-photoblog-title "Blog de Fotos"
            :sections-photoblog-subtitle "fotografia, blog, inspiração"
            :sections-photoblog-desc "Gosto de pensar a fotografia além de equipamentos, técnicas e cores. O que mais me intriga é a história por trás das lentes. Acredito que fotografar seja sobre a maneira única que as pessoas veem e sentem o mundo em sua volta naquele instante. Esse blog é a maneira que encontrei de contar minhas histórias."
            :sections-photoblog-avatar-alt "Ícone do meu blog de fotos"
+
+           :sections-twitter-title "Twitter"
+           :sections-twitter-subtitle "desenvolvimento de software, TI, assuntos variados"
+           :sections-twitter-desc "Meu twitter é com certeza o jeito mais fácil de me contactar. É aonde eu compartilho na maioria das vezes conteúdo sobre tecnologia e desenvolvimento de software, focando em coisas que me impressionaram e que eu acredito que possam ajudar outras pessoas de alguma maneira. Como ele é um microblog, algumas vezes eu compartilho conteúdo não relacionado a tecnologia, mas sim sobre coisas de interesse pessoal como esportes e política."
+           :sections-twitter-avatar-alt "Ícone do twitter"
+
            :back "voltar"
            :view-more "ver mais"}
+
    :en-us {:title-description "consultant developer"
            :lang-button "Português?"
+
            :sections-summary-title "Summary"
            :sections-summary-subtitle "brief description about me"
            :sections-summary-desc "A consultant of software development solving people's problems with code and ideas. Find here all the content I produce in the web and the ways to contact me. Do you like technology, software development, brazilian jiu jitsu or photography? Let's talk."
            :sections-summary-avatar-alt "Junior Ales' portrait"
+
            :sections-photoblog-title "Photo Blog"
            :sections-photoblog-subtitle "photography, blog, inspiration"
            :sections-photoblog-desc "I like to think of photography beyond techniques, colors, focus, depth and patterns. I'm fascinated about the story behind the lens. It's all about the unique way people see and feel the world around them in that given moment. This blog is a way of telling my stories to everyone. The content of the blog is (for now) only in portuguese, but I believe non portuguese speakers will be able to understand my ideas through my photos."
            :sections-photoblog-avatar-alt "Icon of my photo blog"
+
+           :sections-twitter-title "Twitter"
+           :sections-twitter-subtitle "software dev, IT, random stuff"
+           :sections-twitter-desc "My twitter account is for sure the easiest way to contact me. It's the place where I share mostly technology and software development content, focusing on things that impressed me and I think can help other people in some way. As it's a personal microblog sometimes I share content not related to technology at all, instead things of personal interest like sports and politics."
+           :sections-twitter-avatar-alt "Icon of twitter"
+
            :back "back"
            :view-more "view more"}})
 
