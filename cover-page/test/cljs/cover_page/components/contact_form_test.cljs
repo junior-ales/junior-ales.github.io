09a5863a0968db919ccb5fa83cd52fdb8b712476
Junior Ales <edilson.ales.jr@gmail.com>
2016-03-22 14:04:17 -0300

    [cover] adding more test cases for user input

--- cover-page/test/cljs/cover_page/components/contact_form_test.cljs
+++ cover-page/test/cljs/cover_page/components/contact_form_test.cljs
@@ -4,11 +4,18 @@
             [cljs.test :refer-macros [deftest testing is]]))
 
 (deftest send-message-test
-  (let [fake-label #(% {:email-form-error-empty-message "no message"
-                        :email-form-error-empty-address "no email"
+  (let [message-found #(boolean (some #{%1} %2))
+        fake-label #(% {:email-form-error-empty-address "no email"
+                        :email-form-error-empty-message "no message"
                         :email-form-error-wrong-address "wrong email format"})]
     (testing "Input Validator"
       (binding [label fake-label]
-        (is (= ["no message"] (f/validate-input "user@email.com" nil)))
-        (is (= ["no message"] (f/validate-input "user@email.com" "")))
-        (is (= nil            (f/validate-input "user@email.com" "a message")))))))
+        (is (message-found "no message"         (f/validate-input "user@email.com" "")))
+        (is (message-found "no message"         (f/validate-input "user@email.com" nil)))
+        (is (message-found "no email"           (f/validate-input ""               "a message")))
+        (is (message-found "no email"           (f/validate-input nil              "a message")))
+        (is (message-found "wrong email format" (f/validate-input "user"           "a message")))
+        (is (message-found "wrong email format" (f/validate-input "user@email"     "a message")))
+        (is (message-found "wrong email format" (f/validate-input "@email.com"     "a message")))
+        (is (message-found "wrong email format" (f/validate-input "useremail.com"  "a message")))
+        (is (empty?                             (f/validate-input "user@email.com" "a message")))))))
